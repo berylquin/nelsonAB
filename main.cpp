f16f2d6747a6877f73845078a59d0d5ad50221ea
berylquin <43599433+berylquin@users.noreply.github.com>
2018-09-26 09:16:38 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "iostream"
+
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <cstdio>
+#include <ctime>
+
+#include "nelsonc.h"
+#include "nelsonc.cpp"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+
+
+nelsonc test(1);
+test.schritt();
+
+
+
+
+}
---
+++ nelsonc.h
@@ -0,0 +1,36 @@
+#ifndef NELSONC_H
+#define NELSONC_H
+
+#define _USE_MATH_DEFINES
+#include "math.h"
+#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "random"
+
+class nelsonc
+{
+public:
+    nelsonc(int runs);
+
+    void rstart();
+    double gauss(double mean, double stddev);
+    void schritt();
+
+    int n;
+     double start[1];
+     double x0;
+     double pos[1];
+    double r=0.0;
+    double a=2.0;
+    double s=20.0;
+    double dt=0.05;
+    double ti=0.0;
+
+};
+
+#endif // NELSONC_H
